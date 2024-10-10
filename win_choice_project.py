35626e0b7c8cc40a09fcb5b14e50b23bfc9a8253
truuuba <119066126+truuuba@users.noreply.github.com>
2024-10-10 16:49:25 +0300

    Add files via upload

---
+++ win_choice_project.py
@@ -0,0 +1,44 @@
+from win_new_project import *
+
+CTk.set_appearance_mode("dark")
+CTk.set_default_color_theme("green")
+
+class win_choice_project(CTk.CTk):
+    def __init__(self, id_c):
+        super().__init__()
+        self.geometry("1200x700")
+        self.title("ФМ Калькулятор")
+        self.resizable(True, True)
+        self.protocol('WM_DELETE_WINDOW', self._done)
+
+        self.ch_lab = CTk.CTkLabel(master=self, text="Выберите проект или создайте новый")
+        self.ch_lab.grid(row=0, column=0, padx=(0,0))
+
+        self.old_pr_lab = CTk.CTkLabel(master=self, text="Текущие проекты")
+        self.old_pr_lab.grid(row=3, column=0, padx=(0,0))
+        arr = sql.make_arr_nazv(id_c)
+        self.old_pr = CTk.CTkComboBox(master=self, values=arr)                                                  
+        self.old_pr.grid(row=4, column=0, padx=(0,0))
+        
+        #Дописать переход к проекту
+        self.old_pr_but = CTk.CTkButton(master=self, text="Перейти к проекту")
+        self.old_pr_but.grid(row=5, column=0, padx=(20,20), pady=(20,20))
+
+        self.pr = CTk.CTkLabel(master=self, text="Создайте новый проект")
+        self.pr.grid(row=7, column=0, padx=(0,0))
+        self.new_pr_but = CTk.CTkButton(master=self, text="Создать", command=self.new_pr)
+        self.new_pr_but.grid(row=8, column=0, padx=(0,0))
+
+    def _done(self):
+        self.destroy()
+        os.system('main.py')
+        sys.exit(0)
+
+    def new_pr(self):
+        self.withdraw()
+        c = win_new_project()
+        c.mainloop()
+    
+
+
+
