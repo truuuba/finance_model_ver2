13b1da84105da9ca9fa0a8a8efc4777feb025e8e
truuuba <119066126+truuuba@users.noreply.github.com>
2024-10-23 16:50:58 +0300

    Update win_gpr.py

--- win_gpr.py
+++ win_gpr.py
@@ -1,8 +1,7 @@
-from spinbox import Spinbox
 from win_for_obj import *
 
 class GPR_obsh(CTk.CTkScrollableFrame):
-    def __init__(self, master, id_p, entry_prod, entry_zavis, params_r):
+    def __init__(self, master, entry_prod, entry_zavis, params_r):
         super().__init__(master, width=1100, height=500)
         self.w = CTk.CTkLabel(master=self, text="Параметры работ")
         self.w.grid(row=0, column=0, padx=(5,5), pady=(5,5))
@@ -10,7 +9,6 @@ class GPR_obsh(CTk.CTkScrollableFrame):
         self.pr.grid(row=0, column=1, padx=(5,5), pady=(5,5))
         self.zav = CTk.CTkLabel(master=self, text="Зависимости от процессов (пишите номера процессов через пробел)")
         self.zav.grid(row=0, column=2, padx=(5,5), pady=(5,5))
-        params_r = sql.take_obsh_stati(id_p)
         n = len(params_r)
         for i in range(n):
             data_st = sql.take_st_3(params_r[i].id_o)
@@ -33,14 +31,14 @@ class win_for_gpr(CTk.CTk):
         self.id_p = id_p
         self.entry_prod = []
         self.entry_zavis = []
-        self.params_r = []
+        self.params_r = sql.take_obsh_stati(id_p)
 
         self.make_gpr = CTk.CTkLabel(master=self, text="Создание ГПР по общим статьям")
         self.make_gpr.grid(row=0, column=0, padx=(5,5), pady=(5,5))
 
         self.ttle = CTk.CTkLabel(master=self, text="Введите продолжительность и зависимоти по общим статьям")
         self.ttle.grid(row=1, column=0, padx=(5,5), pady=(5,5))
-        self.win_gpr = GPR_obsh(self, self.id_p, self.entry_prod, self.entry_zavis, self.params_r)
+        self.win_gpr = GPR_obsh(self, self.entry_prod, self.entry_zavis, self.params_r)
         self.win_gpr.grid(row=2, column=0, padx=(5,5), pady=(5,5))
 
         self.but_next = CTk.CTkButton(master=self, text="Данные введены", command=self.next_win)
