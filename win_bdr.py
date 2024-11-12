587f061d42b7221460900bd60be3b976cdd83bb6
truuuba <119066126+truuuba@users.noreply.github.com>
2024-11-12 16:45:04 +0300

    Update win_bdr.py

--- win_bdr.py
+++ win_bdr.py
@@ -1,28 +1,30 @@
 import customtkinter as CTk
 from connect_db import sql
+import re
+from tkinter import messagebox as mb
 
 CTk.set_appearance_mode("dark")
 CTk.set_default_color_theme("green")
 
 class win_for_trati(CTk.CTkScrollableFrame):
     def __init__(self, master, bdr_stati, entry_bdr_stati):
-        super().__init__(master, width=1150, height=400)
+        super().__init__(master, width=1000, height=400)
         txt = ""
         cnt = 0
         for i in range(len(bdr_stati)):
-            elem = sql.take_nazv_BDR(bdr_stati[i].id_)
-            if txt != elem.nazv:
-                self.ttle_label = CTk.CTkLabel(master=self, text=(elem.code + " " + elem.nazv))
-                self.ttle_label.grid(row=cnt, column=1, padx=(5,5), pady=(5,5))
+            for j in range(len(bdr_stati[i])):
+                elem = sql.take_nazv_BDR(bdr_stati[i][j].id_)
+                if txt != elem.nazv:
+                    self.ttle_label = CTk.CTkLabel(master=self, text=(elem.code + " " + elem.nazv))
+                    self.ttle_label.grid(row=cnt, column=1, padx=(5,5), pady=(5,5))
+                    cnt += 1
+                txt = elem.nazv
+                self.ttle_date = CTk.CTkLabel(master=self, text=(bdr_stati[i][j].mnt + " " + str(bdr_stati[i][j].yr)))
+                self.ttle_date.grid(row=cnt, column=0, padx=(5,5), pady=(5,5))
+                self.entry_date = CTk.CTkEntry(master=self)
+                self.entry_date.grid(row=cnt, column=2, padx=(5,5), pady=(5,5))
+                entry_bdr_stati.append(self.entry_date)
                 cnt += 1
-            txt = elem.nazv
-            self.ttle_date = CTk.CTkLabel(master=self, text=(bdr_stati[i].mnt + " " + bdr_stati[i].yr))
-            self.ttle_date.grid(row=cnt, column=0, padx=(5,5), pady=(5,5))
-            self.entry_date = CTk.CTkEntry(master=self)
-            self.entry_date.grid(row=cnt, column=2, padx=(5,5), pady=(5,5))
-            entry_bdr_stati.append(self.entry_date)
-            cnt += 1
-
 
 class win_bdr(CTk.CTk):
     def __init__(self, id_p):
@@ -38,10 +40,28 @@ class win_bdr(CTk.CTk):
             self.bdr_stati.append(l)
         self.entry_bdr_stati = [] #массив с чекбоксами
 
-        self.ttle = CTk.CTkLabel(master=self, text="Введите предполагаемые затраты статей по месяцам")
+        self.ttle = CTk.CTkLabel(master=self, text="Введите предполагаемые затраты статей по месяцам для всего проекта")
         self.ttle.grid(row=0, column=0, padx=(5,5), pady=(5,5))
         self.win_input_bdr = win_for_trati(self, self.bdr_stati, self.entry_bdr_stati)
         self.win_input_bdr.grid(row=1, column=0, padx=(5,5), pady=(5,5))
 
+        self.but_next = CTk.CTkButton(master=self, text="Данные введены", command=self.next_win_bdr)
+        self.but_next.grid(row=2, column=0, padx=(5,5), pady=(5,5))
 
-
+    def next_win_bdr(self):
+        patt = r"^[0-9]+$"
+        prov = True
+        for el in self.entry_bdr_stati:
+            match_ds = re.match(patt, el.get())
+            if not match_ds:
+                prov = False
+        # Добавление данных в БД
+        if prov:
+            cnt = 0
+            for i in range(len(self.bdr_stati)):
+                for j in range(len(self.bdr_stati[i])):
+                    sql.update_bdr_obsh(self.entry_bdr_stati[cnt].get(), self.bdr_stati[i][j].id_)
+                    cnt += 1
+            mb.showinfo('ура', 'победа')
+        else:
+            mb.showerror('Ошибка!', 'Были записаны неверные данные в ГПР')
