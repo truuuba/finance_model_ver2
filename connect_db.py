26846eb70188f5b995d04b142c50c3766f5b1a96
truuuba <119066126+truuuba@users.noreply.github.com>
2024-11-15 15:32:28 +0300

    Update connect_db.py

--- connect_db.py
+++ connect_db.py
@@ -64,6 +64,26 @@ class bdr:
         self.yr = yr
         self.mnt = mnt
 
+class bdr_:
+    def __init__(self, id_, id_st, yr, mnt, plan_ds):
+        self.id_ = id_
+        self.id_st = id_st
+        self.yr = yr
+        self.mnt = mnt
+        self.plan_ds = plan_ds
+
+class st_ur1:
+    def __init__(self, id_, code, nazv):
+        self.id_ = id_
+        self.code = code
+        self.nazv = nazv
+
+class bdr_iskl:
+    def __init__(self, id_, id_st, ds):
+        self.id_ = id_
+        self.id_st = id_st
+        self.ds = ds
+
 class Sql:
     def __init__(self, database="FM_model", server=r"NODE2\DBLMSSQLSRV", username="connect_FM_model", password=r"9*%dA6lU&T6)p2PX", driver="ODBC Driver 17 for SQL Server"):
         connectionString = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
@@ -607,6 +627,66 @@ class Sql:
             return False
         else:
             return True
+    
+    def upd_BDR_iskl(self, obsh_st):
+        for el in obsh_st:
+            cursor = self.cnxn.cursor()
+            zapros = "DELETE FROM BDR_iskl WHERE ID_st_obsh = " + str(el.id_) + ";"
+            cursor.execute(zapros)
+            self.cnxn.commit()
+            cursor.close()
+
+    def take_prod_proj(self, id_p):
+        cursor = self.cnxn.cursor()
+        zapros = "SELECT prod FROM project WHERE ID = " + str(id_p) + ";"
+        cursor.execute(zapros)
+        data = cursor.fetchall()
+        return data[0]
+    
+    def take_st3(self, id_obsh):
+        cursor = self.cnxn.cursor()
+        zapros = "SELECT st_3_ur.ID, st_3_ur.Id_st_2, st_3_ur.code, st_3_ur.nazv FROM st_3_ur INNER JOIN obsh_stati ON obsh_stati.Id_st_3 = st_3_ur.ID WHERE obsh_stati.ID = " + str(id_obsh) + ";"
+        cursor.execute(zapros)
+        data = cursor.fetchall()
+        el = stati_ur(data[0], data[1], del_probel(data[2]), del_probel(data[3]))
+        return el
+    
+    def take_st2(self, id_st3):
+        cursor = self.cnxn.cursor()
+        zapros = "SELECT st_2_ur.ID, st_2_ur.Id_st_1, st_2_ur.code, st_2_ur.nazv FROM st_2_ur INNER JOIN st_3_ur ON st_3_ur.Id_st_2 = st_2_ur.ID WHERE st_3_ur.ID = " + str(id_st3) + ";"
+        cursor.execute(zapros)
+        data = cursor.fetchall()
+        el = stati_ur(data[0], data[1], del_probel(data[2]), del_probel(data[3]))
+        return el
+    
+    def take_st1(self, id_st2):
+        cursor = self.cnxn.cursor()
+        zapros = "SELECT st_1_ur.ID, st_1_ur.code, st_1_ur.nazv FROM st_1_ur INNER JOIN st_2_ur ON st_2_ur.Id_st_1 = st_1_ur.ID WHERE st_2_ur.ID = " + str(id_st2) + ";"
+        cursor.execute(zapros)
+        data = cursor.fetchall()
+        el = st_ur1(data[0], del_probel(data[1]), del_probel(data[2]))
+        return el
+
+    def take_BDR_obsh(self, id_obsh):
+        cursor = self.cnxn.cursor()
+        zapros = "SELECT ID, ID_st_obsh, mnt, yr, plan_ds FROM BDR_obsh WHERE ID_st_obsh = " + str(id_obsh) + ";"
+        cursor.execute(zapros)
+        data = cursor.fetchall()
+        datas = []
+        for i in range(len(data)):
+            el = bdr_(data[i][0], data[i][1], data[i][3], data[i][2], data[i][4])
+            datas.append(el)
+        for el in datas:
+            el.mnt = del_probel(el.mnt)
+        return datas
+    
+    def take_BDR_iskl(self, id_obsh):
+        cursor = self.cnxn.cursor()
+        zapros = "SELECT ID, ID_st_obsh, ds FROM BDR_iskl WHERE ID_st_obsh = " + str(id_obsh) + ";"
+        cursor.execute(zapros)
+        data = cursor.fetchall()
+        el = bdr_iskl(data[0], data[1], data[2])
+        return el
 
 
 def make_arr_list(arr):
